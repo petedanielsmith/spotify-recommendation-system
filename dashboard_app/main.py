841f90ea44a9452b09d6ce7e3bfdeb5b4ab07170
Pete <pete@petedanielsmith.co.uk>
2025-12-16 08:57:54 +0000

    Add empty pages for the dashboard app (#4)

---
+++ dashboard_app/main.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+pages = [
+    st.Page("eda.py", title="EDA - Exploratory Data Analysis", icon=":material/bar_chart:"),
+    st.Page("clustering.py", title="Clustering", icon=":material/diversity_3:"),
+    st.Page("prediction_recommendation.py", title="Prediction - Recommendation", icon=":material/genres:"),
+]
+
+pg = st.navigation(pages)
+pg.run()
